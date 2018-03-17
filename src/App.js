2f42aff4227210ba12d8f61a6dcdaf8e35f6d450
Narek Ghevandiani <n.ghevandiani@gmail.com>
2018-03-17 13:26:38 +0400

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Sum from './components/sum';
+// import Gh from './components/gh';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      x: 0,
+      y: 5,
+      s: 'a'
+    };
+
+    setInterval(()=>{
+      this.setState({x: Math.random(), y: Math.random()});
+    }, 1000);
+  }
+
   render() {
+    // const s = this.state.s;
+    const {x, y} = this.state;
     return (
       <div className="App">
-        <p>Hello React World</p>
+        <Sum x={x} y={y} />
+        {/* <p>{s}</p> */}
+        {/* <Gh /> */}
       </div>
     );
   }
